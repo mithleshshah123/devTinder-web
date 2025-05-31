a65e26dfdfd99e7a605a7d144d462012d58b9b85
mithleshshah123 <mithleshshah243@gmail.com>
2025-05-31 18:36:50 +0530

    feat : logout

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -8,6 +8,7 @@ import { BASE_URL } from "../utils/constants";
 const Login = () => {
   const [emailId, setEmailId] = useState("kumar@mail.com");
   const [password, setPassword] = useState("Kumar@12345");
+  const [error,setError] = useState("");
   const dispatch  = useDispatch();
   const navigate = useNavigate();
 
@@ -24,6 +25,7 @@ const Login = () => {
       dispatch(addUser(res.data));
       return navigate("/");
     } catch (err) {
+      setError(err?.response?.data || "Something went wrong!");
       console.error(err);
     }
   };
@@ -57,6 +59,7 @@ const Login = () => {
               />
             </label>
           </div>
+          <p className="text-red-500">{error}</p>
           <div className="card-actions justify-center m-2">
             <button className="btn btn-primary" onClick={handleLogin}>
               Login
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,51 +1,65 @@
+import axios from "axios";
 import React from "react";
-import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
+import { removeUser } from "../utils/userSlice";
 
 const NavBar = () => {
-
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const user = useSelector((store) => store.user);
 
+  const handleLogout = async () => {
+    try {
+      await axios.post(BASE_URL + "/logout");
+      dispatch(removeUser());
+      navigate("/login");
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
 
   return (
     <div className="navbar bg-base-300 shadow-sm">
       <div className="flex-1">
-        <Link to= "/" className="btn btn-ghost text-xl">🧑‍💻DevLink</Link>
+        <Link to="/" className="btn btn-ghost text-xl">
+          🧑‍💻DevLink
+        </Link>
       </div>
-      {user && (<div className="flex gap-2">
-        <div className="form-control mt-2">Welcome, {user.firstName}</div>
-        <div className="dropdown dropdown-end">
-          <div
-            tabIndex={0}
-            role="button"
-            className="btn btn-ghost btn-circle avatar mx-6"
-          >
-            <div className="w-10 rounded-full">
-              <img
-                alt="user photo"
-                src={user.photoUrl}
-              />
+      {user && (
+        <div className="flex gap-2">
+          <div className="form-control mt-2">Welcome, {user.firstName}</div>
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar mx-6"
+            >
+              <div className="w-10 rounded-full">
+                <img alt="user photo" src={user.photoUrl} />
+              </div>
             </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <Link to="/profile" className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </Link>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a onClick={handleLogout}>Logout</a>
+              </li>
+            </ul>
           </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-          >
-            <li>
-              <Link to="/profile" className="justify-between">
-                Profile
-                <span className="badge">New</span>
-              </Link>
-            </li>
-            <li>
-              <a>Settings</a>
-            </li>
-            <li>
-              <a>Logout</a>
-            </li>
-          </ul>
         </div>
-      </div>)}
+      )}
     </div>
   );
 };
