f4cf0457c9c43953da1aae97f29e91d2763260d2
mithleshshah123 <mithleshshah243@gmail.com>
2025-06-01 23:55:12 +0530

    feat: Connection page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import appStore from "./utils/appStore";
 import Body from "./components/Body";
 import Feed from "./components/Feed";
+import Connections from "./components/Connections";
+import Requests from "./components/Requests";
 
 function App() {
   return (
@@ -16,8 +18,8 @@ function App() {
               <Route path="/" element={<Feed />} />
               <Route path="/login" element={<Login />} />
               <Route path="/profile" element={<Profile />} />
-              <Route path="/connections" element={<Profile />} />
-              <Route path="/requests" element={<Profile />} />
+              <Route path="/connections" element={<Connections />} />
+              <Route path="/requests" element={<Requests />} />
             </Route>
           </Routes>
         </BrowserRouter>
