70c21eb09bceae94ae5611dec94438bb79ea53f1
mithleshshah123 <mithleshshah243@gmail.com>
2025-06-02 10:55:27 +0530

    Send/Ignore the user card from the feed

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,23 +1,50 @@
+import axios from "axios";
 import React from "react";
+import { useDispatch } from "react-redux";
+import { removeUserFeed } from "../utils/feedSlice";
+import { BASE_URL } from "../utils/constants";
 
-const UserCard = ({user}) => {
-    const {firstName,lastName,about,age,gender,skills,photoUrl} = user;
+const UserCard = ({ user }) => {
+  const { _id, firstName, lastName, about, age, gender, skills, photoUrl } =
+    user;
+  const dispatch = useDispatch();
+
+  const handleSendRequest = async (status, userId) => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/request/send/" + status + "/" + userId,
+        {},
+        { withCredentials: true }
+      );
+      dispatch(removeUserFeed(userId));
+    } catch (err) {
+      //err
+    }
+  };
+
   return (
     <div className="card bg-base-300 w-96 shadow-sm">
       <figure>
-        <img
-          src={photoUrl}
-          alt="photo"
-        />
+        <img src={photoUrl} alt="photo" />
       </figure>
       <div className="card-body">
         <h2 className="card-title">{firstName + " " + lastName}</h2>
-        {age&&gender&&<p>{age +", " + gender}</p>}
+        {age && gender && <p>{age + ", " + gender}</p>}
         <p>{about}</p>
         <p>{skills}</p>
         <div className="card-actions justify-center my-4">
-          <button className="btn btn-primary">Ignore</button>
-          <button className="btn btn-secondary">Interested</button>
+          <button
+            className="btn btn-primary"
+            onClick={() => handleSendRequest("ignored", _id)}
+          >
+            Ignore
+          </button>
+          <button
+            className="btn btn-secondary"
+            onClick={() => handleSendRequest("interested", _id)}
+          >
+            Interested
+          </button>
         </div>
       </div>
     </div>
--- src/utils/feedSlice.js
+++ src/utils/feedSlice.js
@@ -8,11 +8,12 @@ const feedSlice = createSlice({
       return action.payload;
     },
     // eslint-disable-next-line no-unused-vars
-    removeFeed: (state, action) => {
-      return null;
+    removeUserFeed: (state, action) => {
+      const newFeed = state.filter((user) => user._id !== action.payload);
+      return newFeed;
     },
   },
 });
 
-export const { addFeed, removeFeed } = feedSlice.actions;
+export const { addFeed, removeUserFeed } = feedSlice.actions;
 export default feedSlice.reducer;
